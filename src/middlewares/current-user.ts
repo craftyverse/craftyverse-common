638fef66e511e6b7c75221f578aef2701f5779d0
tonyatlcs <tonyatlcs@gmail.com>
2023-08-08 16:34:23 +1000

    Updates

--- src/middlewares/current-user.ts
+++ src/middlewares/current-user.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 
 interface UserPayload {
-  _id: string;
+  id: string;
   firstName: string;
   lastName: string;
   email: string;
