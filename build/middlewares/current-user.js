b8e53f436026f6b217fa388d5cf33dc97d197885
Tony Li <tonyatlcs@gmail.com>
2023-08-25 11:20:55 +1000

    More robust error logging

--- build/middlewares/current-user.js
+++ build/middlewares/current-user.js
@@ -6,14 +6,16 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.currentUser = void 0;
 const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
 const currentUser = (req, res, next) => {
-    if (!req.session || !req.session.jwt) {
+    var _a;
+    if (!((_a = req.session) === null || _a === void 0 ? void 0 : _a.jwt)) {
         return next();
     }
     try {
         const payload = jsonwebtoken_1.default.verify(req.session.jwt, process.env.JWT_KEY);
+        console.log(req.session);
         req.currentUser = payload;
     }
-    catch (error) { }
+    catch (err) { }
     next();
 };
 exports.currentUser = currentUser;
