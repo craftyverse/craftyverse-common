87f56482181acf1a9c154566690e9d716ebd73a2
Tony Li <tonyatlcs@gmail.com>
2023-08-26 10:30:39 +1000

    More robust error logging

--- build/middlewares/require-auth.js
+++ build/middlewares/require-auth.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.requireAuth = void 0;
 const not_authorised_error_1 = require("../errors/not-authorised-error");
 const requireAuth = (req, res, next) => {
-    console.log(req.currentUser);
+    console.log("current user: ", req.currentUser);
     if (!req.currentUser) {
         throw new not_authorised_error_1.NotAuthorisedError();
     }
