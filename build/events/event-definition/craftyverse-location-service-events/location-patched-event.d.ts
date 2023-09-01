9923b4a358e72e6646cbac4e61be921b4d7e353f
Tony Li <tonyatlcs@gmail.com>
2023-09-01 11:32:03 +1000

    adding modified event definitions

--- build/events/event-definition/craftyverse-location-service-events/location-patched-event.d.ts
+++ build/events/event-definition/craftyverse-location-service-events/location-patched-event.d.ts
@@ -1,19 +1,4 @@
 import { Subjects } from "../../subjects";
-declare enum locationRegionEnum {
-    Australia = "AUS",
-    England = "ENG",
-    America = "USA",
-    China = "CHN"
-}
-declare enum locationSIUnitEnum {
-    Pound = "LB",
-    Kilo = "KG"
-}
-declare enum locationCurrencyEnum {
-    AustralianDollar = "AUD",
-    USDollar = "USD",
-    ChineseDollar = "RMB"
-}
 export interface LocationPatchedEvent {
     subject: Subjects.LocationPatched;
     data: {
@@ -22,10 +7,10 @@ export interface LocationPatchedEvent {
         locationName: string;
         locationEmail: string;
         locationIndustry: string;
-        locationRegion: locationRegionEnum;
-        locationCurrency: locationCurrencyEnum;
+        locationRegion: string;
+        locationCurrency: string;
         locationTimeZone: string;
-        locationSIUnit: locationSIUnitEnum;
+        locationSIUnit: string;
         locationLegalBusinessName: string;
         locationLegalAddressLine1: string;
         locationLegalAddressLine2: string;
@@ -36,4 +21,3 @@ export interface LocationPatchedEvent {
         locationApproved: boolean;
     };
 }
-export {};
--- build/events/event-definition/craftyverse-location-service-events/location-patched-event.js
+++ build/events/event-definition/craftyverse-location-service-events/location-patched-event.js
@@ -1,20 +1,2 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var locationRegionEnum;
-(function (locationRegionEnum) {
-    locationRegionEnum["Australia"] = "AUS";
-    locationRegionEnum["England"] = "ENG";
-    locationRegionEnum["America"] = "USA";
-    locationRegionEnum["China"] = "CHN";
-})(locationRegionEnum || (locationRegionEnum = {}));
-var locationSIUnitEnum;
-(function (locationSIUnitEnum) {
-    locationSIUnitEnum["Pound"] = "LB";
-    locationSIUnitEnum["Kilo"] = "KG";
-})(locationSIUnitEnum || (locationSIUnitEnum = {}));
-var locationCurrencyEnum;
-(function (locationCurrencyEnum) {
-    locationCurrencyEnum["AustralianDollar"] = "AUD";
-    locationCurrencyEnum["USDollar"] = "USD";
-    locationCurrencyEnum["ChineseDollar"] = "RMB";
-})(locationCurrencyEnum || (locationCurrencyEnum = {}));
