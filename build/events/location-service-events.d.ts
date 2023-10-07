4e1791ed7f9533e63bee2d8f3651e5b735d61bde
Tony Li <tonyatlcs@gmail.com>
2023-10-07 17:30:28 +1100

    Added aws sns and sqs functionality

---
+++ build/events/location-service-events.d.ts
@@ -0,0 +1,6 @@
+export declare enum locationEventVariables {
+    LOCATION_CREATED_EVENT = "location-created"
+}
+export declare enum locationEventQueueVariables {
+    LOCATION_CREATED_EVENT_QUEUE = "location-created-queue"
+}
---
+++ src/events/location-service-events.ts
@@ -0,0 +1,7 @@
+export enum locationEventVariables {
+  LOCATION_CREATED_EVENT = "location-created",
+}
+
+export enum locationEventQueueVariables {
+  LOCATION_CREATED_EVENT_QUEUE = "location-created-queue",
+}
