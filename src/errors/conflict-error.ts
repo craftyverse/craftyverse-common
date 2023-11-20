a30fc49bccfdd94ab264b1584baa3656c08b26cb
Tony Li <tonyatlcs@gmail.com>
2023-11-20 15:10:52 +1100

    Added 409 error status

--- src/errors/conflict-error.ts
+++ src/errors/conflict-error.ts
@@ -1,12 +1,12 @@
 import { CustomError } from "./custom-error";
-export class ConfictError extends CustomError {
+export class ConflictError extends CustomError {
   statusCode = 409;
 
   constructor(public message: string) {
     super(message);
 
     // Only because we are extending a built in class
-    Object.setPrototypeOf(this, ConfictError.prototype);
+    Object.setPrototypeOf(this, ConflictError.prototype);
   }
 
   serialiseErrors(): { message: string; field: string | number }[] {
